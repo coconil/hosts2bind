86f417ddf57e176ddd5948edd3f1f55ef2096c6c
Bin Liu <1771263425@qq.com>
2017-08-04 14:30:33 +0800

    hosts to bind

---
+++ README.md
@@ -0,0 +1,4 @@
+# hosts to bind conf
+
+this program read hosts file as input, and output some bind9 server config files.It could be used in your own dns server for 
+the purpose to visit google etc.
---
+++ main.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+import copy
+
+
+class Record:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+
+class Zone:
+    'a name zone'
+
+    def __init__(self, name):
+        self.name = name
+        self.recordList = []
+
+    def insertRecord(self, record):
+        self.recordList.append(record)
+
+    def __contains__(self, record):
+        return record in self.recordList
+
+
+def parseZoneName(name):
+    index = name.rfind('.com')
+    if index < 0:
+        return None
+    index = name.rfind('.', 0, index)
+    # print('zone name:', name[index + 1:])
+    return name[index + 1:]
+
+
+def getZoneFromName(list, name):
+    for zone in list:
+        if zone.name == name:
+            return zone
+    return None
+
+
+file = open('hosts')
+zoneList = []
+
+while True:
+    line = file.readline()
+
+    if not line:
+        break
+    index = line.find(' ')
+    if index < 0:
+        continue
+    address = line[0:index]
+    while line[index] == ' ':
+        index += 1
+    name = line[index:len(line) - 1]
+    tempZoneName = parseZoneName(name)
+    if not tempZoneName:
+        continue
+    tempZone = getZoneFromName(zoneList, tempZoneName)
+    if tempZone:
+        tempRecordName = name[:name.find(tempZoneName) - 1]
+        if not (name in tempZone):
+            tempZone.insertRecord(Record(name[:name.find(tempZoneName) - 1], address))
+    else:
+        tempZone = Zone(tempZoneName)
+        tempZone.insertRecord(Record(name[:name.find(tempZoneName) - 1], address))
+        zoneList.append(tempZone)
+zonefile = open('myzone.conf', 'w+')
+for zone in zoneList:
+
+    print('\n' + zone.name + ': ')
+    zonefile.write(' zone '+'"'+zone.name+'" '+'''{
+type master;
+file "/etc/bind/db.'''+zone.name+'''";
+};
+''')
+    outfile = open('db.' + zone.name, 'w+')
+    outfile.write('''
+$TTL 7200
+; domain.tld
+@       IN      SOA     ns.lab.cn. ns2.lab.cn. (
+                                        2017071401 ; Serial
+                                        28800      ; Refresh
+                                        1800       ; Retry
+                                        604800     ; Expire - 1 week
+                                        86400 )    ; Minimum
+@       IN      NS      ns.lab.cn.
+''')
+    for record in zone.recordList:
+        outfile.write(record.name + '     IN      A       ' + record.address + '\n')
+        print(record.name, record.address)
+    outfile.close()
+zonefile.close()
+    # print('address:', address, ' name:', name)
